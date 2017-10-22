6b08be17bdaa994bf2d099d867ca63752b083ffe
torpedo87 <bestofhand@naver.com>
2017-10-22 10:29:02 +0900

    GoalsVC UI 만들기

---
+++ GoalPost/Controller/GoalsVC.swift
@@ -0,0 +1,21 @@
+//
+//  GoalsVC.swift
+//  GoalPost
+//
+//  Created by junwoo on 2017. 10. 22..
+//  Copyright © 2017년 samchon. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func addGoalBtnPressed(_ sender: Any) {
+  }
+}
